172ac300c686d267997fb462a2097febe8459fe3
LIEHN-Noam <120740747+LIEHN-Noam@users.noreply.github.com>
2023-06-16 13:24:26 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+$(document).ready(function (){
+    $("#mini").hover(function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/miniloupe.png').fadeIn(200);
+            });
+        },function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/mini.png').fadeIn(200);
+            });
+        });
+})
+$(document).ready(function (){
+    $("#coca").hover(function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/cocaloupe.png').fadeIn(200);
+            });
+        },function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/coca-cola.png').fadeIn(200);
+            });
+        });
+})
+$(document).ready(function (){
+    $("#mgm").hover(function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/mgmloupe.png').fadeIn(200);
+            });
+        },function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/MGM.png').fadeIn(200);
+            });
+        });
+})
+$(document).ready(function (){
+    $("#saucony").hover(function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/sauconyloupe.png').fadeIn(200);
+            });
+        },function(){
+          $(this).fadeOut(200, function(){
+            $(this).attr('src', 'img/saucony.png').fadeIn(200);
+            });
+        });
+})
